c67444541ebb71b0c91ff711fedc86656f310010
K V S SIVA SURYA VITAL <84788177+Vital-Kancharla@users.noreply.github.com>
2025-09-04 22:27:59 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -35,7 +35,7 @@ const Login = () => {
           password
         };
         try{
-        const response = await axios.post("http://localhost:3000/api/v1/register", formData);
+        const response = await axios.post("https://mern-login-ache.onrender.com/api/v1", formData);
          toast.success("Registration successfull");
          navigate("/login");
        }catch(err){
