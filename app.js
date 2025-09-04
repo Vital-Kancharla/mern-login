83bdd4c8f656cf5a59f5ed736b173f0c97f15c4c
K V S SIVA SURYA VITAL <vitalnaidu9959@gmail.com>
2025-09-04 20:39:41 +0530

    Fix syntax errors in app.js

--- app.js
+++ app.js
@@ -25,16 +25,15 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
 
-    try {        
+    try {
         await connectDB(process.env.MONGO_URI);
         app.listen(port, () => {
             console.log(`Server is listening on port ${port}`);
         })
 
     } catch (error) {
-       console.log(error); 
+       console.log(error);
     }
 }
 
 start();
-
